a486135112729fe3734ea34728574f5fb432a286
Jesse <client@Jesse.local>
2022-08-07 01:21:29 +0100

    update website

--- src/Styles/Hero/Hero.jsx
+++ src/Styles/Hero/Hero.jsx
@@ -19,7 +19,7 @@ function Hero() {
                 <HeroImages>
                     <HeroCard>
                         <HeroImage>
-                            <img src="/Header.png" alt="Header-1" style={{ width: "90%" }} />
+                            <img src="/Header2.png" alt="Header-1" style={{ width: "90%" }} />
                         </HeroImage>
                         <CardDetails>
                             <CardTitle><CardTitleText>"Rainbow Contemplates ...</CardTitleText><Chip></Chip></CardTitle>
